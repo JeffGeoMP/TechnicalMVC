53f11924493bee073defff0ab29c5e618eaee915
Jefferson Geovanny <jeffgeomp@gmail.com>
2024-03-09 01:49:48 -0600

    feat: initial project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnicalMVC.Models.Entities;
+
+namespace TechnicalMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ Models/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalMVC.Models.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DPI { get; set; }
+        public string NIT { get; set; }
+        public string Address { get; set; }
+    }
+}
